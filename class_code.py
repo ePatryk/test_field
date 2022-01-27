2e7c1fe99bca06775ee69cfc39edc80ace441157
ePatryk <pat.kal1989@gmail.com>
2022-01-27 19:27:19 +0100

    zmiana pliku na .py

---
+++ class_code.py
@@ -0,0 +1,23 @@
+class Movie():
+   
+    def __init__(self, title, storyline, hero, trailer):
+       
+        self.title=title
+        self.storyline=storyline
+        self.hero=hero
+        self.trailer=trailer
+        
+    def config(self):
+        
+        print(self.title, self.storyline, self.hero, self.trailer)
+        
+Matrix = Movie("Matrix", "Action", "Neo", "yt:Matrix")
+
+Matrix.config()
+
+
+Southpark = Movie("Miasteczko Southpark", "Comedy", "Kenny", "yt:Southpark")
+
+
+Southpark.config()
+
